62f89a1caf2c100a2e97bb8c166b43bd0c1286cb
cinnamon_rowll <166984309+pwepperspray@users.noreply.github.com>
2024-10-10 02:25:25 +0530

    Cleaned up code and files

--- src/playerEntity.cpp
+++ src/playerEntity.cpp
@@ -1,6 +1,7 @@
 #include "entity.h"
 
 PlayerEntity::PlayerEntity(sf::Texture* playerTexture, sf::Vector2f m_playerPos, sf::Vector2f m_playerScale){
+	this->m_playerPos = m_playerPos;
 	playerSprite.setTexture(*playerTexture);
 	playerSprite.setPosition(m_playerPos);
 	playerSprite.scale(m_playerScale);
@@ -11,3 +12,31 @@ PlayerEntity::PlayerEntity(sf::Texture* playerTexture, sf::Vector2f m_playerPos,
 void PlayerEntity::playerDraw(sf::RenderWindow &window){
 	window.draw(playerSprite);
 }
+
+void PlayerEntity::playerMove(char direction){
+	switch(direction){
+		case 'L':
+			playerSprite.move(-1,0);
+			break;
+		case 'R':
+			playerSprite.move(1,0);
+			break;
+		default:
+			break;
+	}
+}
+
+#include "entity.h"
+
+EnemyEntity::EnemyEntity(sf::Texture* enemyTexture,int level, sf::Vector2f m_enemyPos, sf::Vector2f m_enemyScale){
+	enemySprite.setTexture(*enemyTexture);
+	enemySprite.setPosition(m_enemyPos);
+	enemySprite.scale(m_enemyScale);
+	sf::Vector2u textureSize = enemySprite.getTexture()->getSize();
+	enemySprite.setOrigin(textureSize.x / 2, textureSize.y / 2);	
+}
+
+void EnemyEntity::enemyDraw(sf::RenderWindow &window){
+	window.draw(enemySprite);
+}
+
